6454802d68e8ea7cb7f15784f57ed5b6ffabb760
Minh Mai <minhmanmai90@gmail.com>
2020-01-04 18:23:18 +1100

    add ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,22 @@ import classes from './Burger.module.css';
 import Ingredient from './Ingredients/Ingredient';
 
 const Burger = (props) => {
+    let ingredients = Object.keys(props.ingredients)
+    .map(ingredientKey => {
+        return [...Array(props.ingredients[ingredientKey])].map((_, index) => {
+            return <Ingredient key={ingredientKey + 1} type={ingredientKey}/>;
+        })
+    }).reduce((arr, el) => {
+        return arr.concat(el);
+    }, [])
+    if(ingredients.length === 0) {
+        ingredients = <p>Please add ingredient!</p>
+    }
+
     return(
         <div className={classes.Burger}>
             <Ingredient type="bread-top"/>
-            <Ingredient type="cheese"/>
-            <Ingredient type="meat"/>
+            {ingredients}
             <Ingredient type="bread-bottom"/>
         </div>
     );
