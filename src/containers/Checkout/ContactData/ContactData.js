4a9c7d4501b916fde48d22c6bd2e9229a308b4f9
Minh Mai <minhmanmai90@gmail.com>
2020-02-28 15:40:17 +1100

    use token on ordering burger

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -106,7 +106,7 @@ class ContactData extends Component {
             orderData: formData
         }
 
-        this.props.onOrderBurger(order);
+        this.props.onOrderBurger(order, this.props.token);
     };
 
     checkValidity = (value, rules) => {
@@ -183,13 +183,14 @@ const mapStateToProps = state => {
     return {
         ingredients: state.burgerBuilder.ingredients,
         totalPrice: state.burgerBuilder.totalPrice,
-        loading: state.order.loading
+        loading: state.order.loading,
+        token: state.auth.token
     }
 }
 
 const mapDispatchtoProps = dispatch => {
     return {
-    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))}
+    onOrderBurger: (orderData, token) => dispatch(actions.purchaseBurger(orderData, token))}
 }
 
 export default connect(mapStateToProps, mapDispatchtoProps)(withErrorHandler(ContactData, axios));
