a7374958180c4fe83ff7b3a4790d07090f68d5e0
Minh Mai <minhmanmai90@gmail.com>
2020-03-05 21:34:21 +1100

    test cases for auth reducer

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -5,24 +5,22 @@ import Adapter from 'enzyme-adapter-react-16';
 import NavigationItems from './NavigationItems';
 import NavigationItem from './NavigationItem/NavigationItem';
 
-configure({
-    adapter: new Adapter()
-});
+configure({ adapter: new Adapter() });
 
 describe('<NavigationItems/>', () => {
-    let wrapper = 
-    beforeEach(() => {
-        wrapper = shallow(<NavigationItems/>);
-    });
+    let wrapper =
+        beforeEach(() => {
+            wrapper = shallow(<NavigationItems />);
+        });
     it('should render two <NavigationItem/> elements if not authenticated', () => {
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
     });
     it('should render three <NavigationItem/> elements if authenticated', () => {
-        wrapper.setProps({isAuthenticated: true});
+        wrapper.setProps({ isAuthenticated: true });
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
     it('should contain logout <NavigationItem/> elements if authenticated', () => {
-        wrapper.setProps({isAuthenticated: true});
+        wrapper.setProps({ isAuthenticated: true });
         expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
     });
 });
